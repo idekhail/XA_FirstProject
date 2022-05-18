09474e4d37940f67cfeadeb967fd74e317e20c7e
Ibrahim AlDekhail <33824044+idekhail@users.noreply.github.com>
2022-05-18 12:01:31 +0300

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android;
+
+namespace XA_FirstProject
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            Xamarin.Essentials.Platform.Init(this, bundle);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+
+            Button btn = FindViewById<Button>(Resource.Id.MyButton);
+            TextView txtDisp= FindViewById<TextView>(Resource.Id.TxtDisp);
+
+            btn.Click += delegate
+            {
+                txtDisp.Text = " Ahmed";
+            };
+
+        }
+       
+    }
+}
